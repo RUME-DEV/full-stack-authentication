f6ba8c6cabb1d53f9d3fdb882d7a99497539b7d4
Peace A. <320020099+RUME-DEV@users.noreply.github.com>
2026-08-27 23:20:30 +0100

    feat: copy feature added to home

--- frontend/src/Components/Welcome.jsx
+++ frontend/src/Components/Welcome.jsx
@@ -1,10 +1,10 @@
 import styles from "./Component.module.css"
 
-function Welcome({username, name, email, id}) {
+function Welcome({username, name, email, id, copyId}) {
     return (
         <div className={styles.welcome}>
             <h1>Welcome {username}</h1>
-            <p>{id ? `Id: ${id}` : null}</p>
+            <p onClick={copyId}>{id ? `Id: ${id}` : null}</p>
             <p>{name ? `Name: ${name}` : null}</p>
             <p>email: {email}</p>
         </div>
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,20 +1,41 @@
-import { useContext, useEffect } from "react"
-import { Link } from "react-router-dom"
+import { useContext, useState } from "react"
 import { AuthContext } from "../Context/AuthContext"
 import Welcome from "../Components/Welcome"
 import styles from "./Pages.module.css"
 
 function Home() {
     const { user } = useContext(AuthContext)
+
+    const [status, setStatus] = useState("")
     console.log(`Home: `, user)
+
+    const copyId = async() => {
+        try {
+            await navigator.clipboard.writeText(user?.id)
+
+            setTimeout(() => {
+                setStatus("Id Copied")
+            }, 1500);
+        } catch(err) {
+            setStatus("Error Copying Id")
+        }
+    }
+
+    if (status) {
+        setTimeout(() => {
+            setStatus("")
+        }, 5000)
+    }
 
     return (
         <div className={styles.home} >   
             <h1>Home Page</h1>
+            <p>{status}</p>
             <Welcome 
             id={user?.id}
             username={user?.username}
             email={user?.email}
+            copyId={copyId}
             />
         </div>
     )
